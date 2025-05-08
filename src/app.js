b32560cf7662171b18cddc53b03624e32b0ab4c8
henryjre <pinedahenryjre@gmail.com>
2025-05-08 20:06:26 +0800

    added cookieparser with signature

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 // import morgan from "morgan";
 import routes from "./router/index.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.use(cors());
 app.use(express.json());
