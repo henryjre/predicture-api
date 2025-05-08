6eb177fe630e0b23c548445ac1ae5a1cd1dc3b85
henryjre <pinedahenryjre@gmail.com>
2025-05-08 20:07:19 +0800

    Add authentication middleware to user data and token routes; introduce trade event endpoint

--- src/router/api/routes/privateRoutes.js
+++ src/router/api/routes/privateRoutes.js
@@ -4,13 +4,18 @@ import {
   createUserData,
   rotateToken,
 } from "../../../controllers/userController.js";
+import { eventReceive } from "../../../controllers/eventController.js";
+import { authenticateApiKey } from "../../../middleware/authentication.js";
 
 const router = express.Router();
 
 // POST /api/private/createUserData
-router.post("/createUserData", createUserData);
+router.post("/createUserData", authenticateApiKey, createUserData);
 
 // POST /api/private/rotateToken
-router.post("/rotateToken", rotateToken);
+router.post("/rotateToken", authenticateApiKey, rotateToken);
+
+// POST /api/events/trade
+router.post("/trade", eventReceive);
 
 export default router;
