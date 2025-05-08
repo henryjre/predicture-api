2473977d8db141a4a4ec21943f5a0e4b381a2820
henryjre <pinedahenryjre@gmail.com>
2025-05-08 15:39:27 +0800

    removed trust proxy

--- src/app.js
+++ src/app.js
@@ -1,16 +1,11 @@
 import express from "express";
 import cors from "cors";
 // import morgan from "morgan";
-import cookieParser from "cookie-parser";
 import routes from "./router/index.js";
 
 const app = express();
 
-// Trust proxy - needed for rate limiter to work behind a proxy
-app.set("trust proxy", 1);
-
 app.use(cors());
-app.use(cookieParser());
 app.use(express.json());
 // app.use(morgan("dev"));
 
